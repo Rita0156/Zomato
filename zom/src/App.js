c1a4dd49b2c3b161f72f685d15a9f984ede041d7
Rita0156 <ritachaudhari006@gmail.com>
2023-09-16 21:26:36 +0530

    mm

--- zom/src/App.js
+++ zom/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Signin from "./Pages/Signin"
 
 function App() {
   return (
     <div className="App">
-      
+      <Signin/>
     </div>
   );
 }
--- zom/src/Pages/Signin.jsx
+++ zom/src/Pages/Signin.jsx
@@ -1,8 +1,15 @@
 function Signin(){
     return (
-        <div>Sign Up
-            
+        <div>
+            <h1>Sign Up</h1>
+            <div style={{display:'flex',justifyContent:"space-between",alignItems:'center',margin:"auto",width:"30%", padding:"20px"}}>
+                <img style={{width:"10%"}} src="https://ww1.freelogovectors.net/wp-content/uploads/2023/03/facebook-logo-new-2019-freelogovectors.net_.png?lossy=1&ssl=1" alt="" />
+                <img style={{width:"10%"}} src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="" />
+                <img style={{width:"8%"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="" />
+            </div>
         </div>
         
     )
 }
+
+export default Signin
