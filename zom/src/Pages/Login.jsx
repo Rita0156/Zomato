90e93fdc3b68bcfd6b04127f25369288e2d66486
Rita0156 <ritachaudhari006@gmail.com>
2023-09-16 21:31:34 +0530

    log

---
+++ zom/src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+
+function Login(){
+    return (
+        <div>
+            <h1>Login</h1>
+            <div>
+                <form>
+                    <input type="text" placeholder="Enter Name"/><br/>
+                    <input type="password" placeholder="Enter Password"/>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
